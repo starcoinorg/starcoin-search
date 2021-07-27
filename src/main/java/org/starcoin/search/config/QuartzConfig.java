20582287e78883df78352f4047e7e5b1c950c01f
chengsuoyuan <suoyuan@gmail.com>
2021-07-27 15:41:30 +0800

    [deploy]update k8s config and add bulk config

--- src/main/java/org/starcoin/search/config/QuartzConfig.java
+++ src/main/java/org/starcoin/search/config/QuartzConfig.java
@@ -37,7 +37,6 @@ public class QuartzConfig {
         try {
             schedulerFactoryBean.setQuartzProperties(quartzProperties());
             schedulerFactoryBean.setJobFactory(searchJobFactory);
-//            schedulerFactoryBean.setTriggers(startQuartzTrigger());
         } catch (Exception e) {
             e.printStackTrace();
         }

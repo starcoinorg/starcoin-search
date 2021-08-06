5e16b5b3a1d3b7ff5588f0359c5d16fbc37c2b59
chengsuoyuan <suoyuan@gmail.com>
2021-08-06 09:12:36 +0800

    [transfer]add secondary indexer and address holder

--- src/main/java/org/starcoin/search/handler/ElasticSearchHandler.java
+++ src/main/java/org/starcoin/search/handler/ElasticSearchHandler.java
@@ -1,15 +1,20 @@
 package org.starcoin.search.handler;
 
 import com.alibaba.fastjson.JSON;
+import com.novi.serde.Bytes;
+import com.novi.serde.DeserializationError;
+import org.bouncycastle.util.Arrays;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.*;
+import org.elasticsearch.client.indices.GetMappingsRequest;
+import org.elasticsearch.client.indices.GetMappingsResponse;
+import org.elasticsearch.client.indices.PutMappingRequest;
 import org.elasticsearch.cluster.metadata.MappingMetadata;
-import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
@@ -17,49 +22,44 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.starcoin.api.StateRPCClient;
 import org.starcoin.bean.*;
 import org.starcoin.search.bean.Offset;
+import org.starcoin.search.constant.Constant;
+import org.starcoin.types.AccountAddress;
 import org.starcoin.types.StructTag;
+import org.starcoin.types.event.DepositEvent;
 import org.starcoin.utils.Hex;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class ElasticSearchHandler {
 
-    private static final String BLOCK_INDEX = "block_ids";
-    private static final String BLOCK_CONTENT_INDEX = "blocks";
-    private static final String UNCLE_BLOCK_INDEX = "uncle_blocks";
-    private static final String TRANSACTION_INDEX = "txn_infos";
-    private static final String EVENT_INDEX = "txn_events";
-    private static final String PENDING_TXN_INDEX = "pending_txns";
-    private static final String TRANSFER_INDEX = "transfer";
-    private static final int ELASTICSEARCH_MAX_HITS = 10000;
-
     private static final Logger logger = LoggerFactory.getLogger(ElasticSearchHandler.class);
     private final RestHighLevelClient client;
+    private final StateRPCClient stateRPCClient;
     @Value("${starcoin.network}")
     private String network;
 
-    public ElasticSearchHandler(RestHighLevelClient client) {
+    public ElasticSearchHandler(RestHighLevelClient client, StateRPCClient stateRPCClient) {
         this.client = client;
+        this.stateRPCClient = stateRPCClient;
     }
 
     @PostConstruct
     public void initIndexes() {
         logger.info("init indices...");
         try {
-            createIndexIfNotExist(BLOCK_INDEX);
-            createIndexIfNotExist(BLOCK_CONTENT_INDEX);
-            createIndexIfNotExist(UNCLE_BLOCK_INDEX);
-            createIndexIfNotExist(TRANSACTION_INDEX);
-            createIndexIfNotExist(EVENT_INDEX);
-            createIndexIfNotExist(PENDING_TXN_INDEX);
-            createIndexIfNotExist(TRANSFER_INDEX);
+            ServiceUtils.createIndexIfNotExist(client, network, Constant.BLOCK_IDS_INDEX);
+            ServiceUtils.createIndexIfNotExist(client, network, Constant.BLOCK_CONTENT_INDEX);
+            ServiceUtils.createIndexIfNotExist(client, network, Constant.UNCLE_BLOCK_INDEX);
+            ServiceUtils.createIndexIfNotExist(client, network, Constant.TRANSACTION_INDEX);
+            ServiceUtils.createIndexIfNotExist(client, network, Constant.EVENT_INDEX);
+            ServiceUtils.createIndexIfNotExist(client, network, Constant.PENDING_TXN_INDEX);
+            ServiceUtils.createIndexIfNotExist(client, network, Constant.TRANSFER_INDEX);
             logger.info("index init ok!");
         } catch (IOException e) {
             logger.error("init index error:", e);
@@ -69,7 +69,7 @@ public class ElasticSearchHandler {
     public Offset getRemoteOffset() {
         GetMappingsRequest request = new GetMappingsRequest();
         try {
-            String offsetIndex = ServiceUtils.getIndex(network, BLOCK_CONTENT_INDEX);
+            String offsetIndex = ServiceUtils.getIndex(network, Constant.BLOCK_CONTENT_INDEX);
             request.indices(offsetIndex);
             GetMappingsResponse response = client.indices().getMapping(request, RequestOptions.DEFAULT);
             MappingMetadata data = response.mappings().get(offsetIndex);
@@ -87,7 +87,7 @@ public class ElasticSearchHandler {
     }
 
     public void setRemoteOffset(Offset offset) {
-        String offsetIndex = ServiceUtils.getIndex(network, BLOCK_CONTENT_INDEX);
+        String offsetIndex = ServiceUtils.getIndex(network, Constant.BLOCK_CONTENT_INDEX);
         PutMappingRequest request = new PutMappingRequest(offsetIndex);
         try {
             XContentBuilder builder = XContentFactory.jsonBuilder();
@@ -120,13 +120,13 @@ public class ElasticSearchHandler {
             return;
         }
         BulkRequest bulkRequest = new BulkRequest();
-        String blockIndex = ServiceUtils.getIndex(network, BLOCK_INDEX);
-        String blockContentIndex = ServiceUtils.getIndex(network, BLOCK_CONTENT_INDEX);
-        String uncleIndex = ServiceUtils.getIndex(network, UNCLE_BLOCK_INDEX);
-        String txnIndex = ServiceUtils.getIndex(network, TRANSACTION_INDEX);
-        String eventIndex = ServiceUtils.getIndex(network, EVENT_INDEX);
-        String pendingIndex = ServiceUtils.getIndex(network, PENDING_TXN_INDEX);
-        String transferIndex = ServiceUtils.getIndex(network, TRANSFER_INDEX);
+        String blockIndex = ServiceUtils.getIndex(network, Constant.BLOCK_IDS_INDEX);
+        String blockContentIndex = ServiceUtils.getIndex(network, Constant.BLOCK_CONTENT_INDEX);
+        String uncleIndex = ServiceUtils.getIndex(network, Constant.UNCLE_BLOCK_INDEX);
+        String txnIndex = ServiceUtils.getIndex(network, Constant.TRANSACTION_INDEX);
+        String eventIndex = ServiceUtils.getIndex(network, Constant.EVENT_INDEX);
+        String pendingIndex = ServiceUtils.getIndex(network, Constant.PENDING_TXN_INDEX);
+        String transferIndex = ServiceUtils.getIndex(network, Constant.TRANSFER_INDEX);
         for (Block block : blockList) {
             //transform difficulty
             BlockHeader header = block.getHeader();
@@ -150,7 +150,7 @@ public class ElasticSearchHandler {
             IndexRequest blockContent = new IndexRequest(blockContentIndex);
             blockContent.id(block.getHeader().getBlockHash()).source(JSON.toJSONString(block), XContentType.JSON);
             bulkRequest.add(blockContent);
-            //add txns
+            //add transactions
             for (Transaction transaction : block.getTransactionList()) {
                 IndexRequest transactionReq = new IndexRequest(txnIndex);
                 transactionReq.id(transaction.getTransactionHash()).source(JSON.toJSONString(transaction), XContentType.JSON);
@@ -162,14 +162,24 @@ public class ElasticSearchHandler {
                 //add events
                 List<Event> events = transaction.getEvents();
                 if (events != null && events.size() > 0) {
+                    Set<AddressHolder> holderAddress = new HashSet<>();
                     for (Event event : events) {
-                        bulkRequest.add(buildEventRequest(event, transaction.getTimestamp(), eventIndex));
+                        bulkRequest.add(buildEventRequest(event, transaction.getTimestamp(), eventIndex, holderAddress));
+                    }
+                    if (!holderAddress.isEmpty()) {
+                        for (AddressHolder holder : holderAddress
+                        ) {
+                            long amount = stateRPCClient.getAddressAmount(holder.address);
+                            bulkRequest.add(buildHolderRequest(holder, amount));
+                        }
                     }
                 }
                 //add transfer
-                IndexRequest transferRequest = buildTransferRequest(transaction, transferIndex);
-                if (transferRequest != null) {
-                    bulkRequest.add(transferRequest);
+                List<IndexRequest> transferRequests = buildTransferRequest(transaction, transferIndex);
+                if (!transferRequests.isEmpty()) {
+                    for (IndexRequest request : transferRequests) {
+                        bulkRequest.add(request);
+                    }
                 }
             }
             //add uncles
@@ -278,42 +288,97 @@ public class ElasticSearchHandler {
         return request;
     }
 
-    private IndexRequest buildTransferRequest(Transaction transaction, String indexName) {
+    private List<IndexRequest> buildTransferRequest(Transaction transaction, String indexName) {
+        List<IndexRequest> requests = new ArrayList<>();
         UserTransaction userTransaction = transaction.getUserTransaction();
         if (userTransaction == null) {
-            return null;
+            return requests;
         }
         RawTransaction rawTransaction = userTransaction.getRawTransaction();
         if (rawTransaction == null) {
-            return null;
+            return requests;
         }
         org.starcoin.types.TransactionPayload payload = rawTransaction.getTransactionPayload();
         if (!(payload.getClass() == org.starcoin.types.TransactionPayload.ScriptFunction.class)) {
             //todo script and package handle
             logger.warn("other type must handle in future: {}", payload.getClass());
-            return null;
+            return requests;
         }
         org.starcoin.types.ScriptFunction function = ((org.starcoin.types.TransactionPayload.ScriptFunction) payload).value;
-        if (function.function.value.equals("peer_to_peer") || function.function.value.equals("peer_to_peer_v2")) {
+        String functionName = function.function.value;
+        if (functionName.equals("peer_to_peer") || functionName.equals("peer_to_peer_v2")) {
             Transfer transfer = new Transfer();
             transfer.setTxnHash(transaction.getTransactionHash());
             transfer.setSender(rawTransaction.getSender());
             transfer.setTimestamp(transaction.getTimestamp());
-            transfer.setIdentifier(function.function.value);
+            transfer.setIdentifier(functionName);
             transfer.setReceiver(Hex.encode(function.args.get(0)));
             String amount = Hex.encode(function.args.get(1));
-            if (function.function.value.equals("peer_to_peer")) {
+            if (functionName.equals("peer_to_peer")) {
                 amount = Hex.encode(function.args.get(2));
             }
             transfer.setAmount(amount);
             transfer.setTypeTag(getTypeTags(function.ty_args));
             IndexRequest request = new IndexRequest(indexName);
             request.source(JSON.toJSONString(transfer), XContentType.JSON);
-            return request;
+            requests.add(request);
+            return requests;
+        } else if (functionName.equals("batch_peer_to_peer") || functionName.equals("batch_peer_to_peer_v2")) {
+            //batch handle
+            Bytes addresses = function.args.get(0);
+            byte[] addressBytes = addresses.content();
+            byte[] amountBytes = function.args.get(2).content();
+            if (functionName.equals("batch_peer_to_peer_v2")) {
+                amountBytes = function.args.get(1).content();
+            }
+            int size = addressBytes.length / AccountAddress.LENGTH;
+            for (int i = 0; i < size; i++) {
+                byte[] addressByte = Arrays.copyOfRange(addressBytes, i * AccountAddress.LENGTH, (i + 1) * AccountAddress.LENGTH);
+                AccountAddress address = AccountAddress.valueOf(addressByte);
+                byte[] amountByte = Arrays.copyOfRange(amountBytes, i * AccountAddress.LENGTH, (i + 1) * AccountAddress.LENGTH);
+                String amount = Hex.encode(amountByte);
+                Transfer transfer = new Transfer();
+                transfer.setTxnHash(transaction.getTransactionHash());
+                transfer.setSender(rawTransaction.getSender());
+                transfer.setTimestamp(transaction.getTimestamp());
+                transfer.setIdentifier(functionName);
+                transfer.setReceiver(address.toString());
+                transfer.setAmount(amount);
+                transfer.setTypeTag(getTypeTags(function.ty_args));
+                IndexRequest request = new IndexRequest(indexName);
+                request.source(JSON.toJSONString(transfer), XContentType.JSON);
+                requests.add(request);
+            }
+            logger.info("batch transfer handle: {}", size);
         } else {
             logger.warn("other scripts not support: {}", function.function.value);
-            return null;
         }
+        return requests;
+    }
+
+    private UpdateRequest buildHolderRequest(AddressHolder holder, long amount) {
+        String addressIndex = ServiceUtils.getIndex(network, Constant.ADDRESS_INDEX);
+        try {
+            XContentBuilder builder = XContentFactory.jsonBuilder();
+            builder.startObject();
+            {
+                builder.field("address", holder.address);
+                builder.field("type_tag", holder.tokenCode);
+                builder.field("amount", amount);
+            }
+            builder.endObject();
+            IndexRequest indexRequest = new IndexRequest(addressIndex);
+            indexRequest.id(holder.address).source(builder);
+            UpdateRequest updateRequest = new UpdateRequest();
+            updateRequest.index(addressIndex);
+            updateRequest.id(holder.address);
+            updateRequest.doc(builder);
+            updateRequest.upsert(indexRequest);
+            return updateRequest;
+        } catch (IOException e) {
+            logger.error("build holder error:", e);
+        }
+        return null;
     }
 
     private String getTypeTags(List<org.starcoin.types.TypeTag> typeTags) {
@@ -321,23 +386,24 @@ public class ElasticSearchHandler {
             return null;
         }
         StringBuilder sb = new StringBuilder();
-        for( org.starcoin.types.TypeTag typeTag: typeTags) {
-            if (typeTag.getClass()== org.starcoin.types.TypeTag.Struct.class ) {
+        for (org.starcoin.types.TypeTag typeTag : typeTags) {
+            if (typeTag.getClass() == org.starcoin.types.TypeTag.Struct.class) {
                 StructTag structTag = ((org.starcoin.types.TypeTag.Struct) typeTag).value;
                 sb.append(structTag.address).append("::").append(structTag.module).append("::").append(structTag.name).append(";");
             }
         }
-        if(sb.length() > 1) {
+        if (sb.length() > 1) {
             sb.deleteCharAt(sb.length() - 1);
         }
         return sb.toString();
     }
 
-    private IndexRequest buildEventRequest(Event event, long timestamp, String indexName) {
+    private IndexRequest buildEventRequest(Event event, long timestamp, String indexName, Set<AddressHolder> holders) {
         IndexRequest request = new IndexRequest(indexName);
         XContentBuilder builder = null;
         try {
             Struct struct = Struct.fromRPC(event.getTypeTag());
+            String eventAddress = event.eventCreateAddress();
             builder = XContentFactory.jsonBuilder();
             builder.startObject();
             builder.field("event_seq_number", event.getEventSeqNumber());
@@ -348,11 +414,29 @@ public class ElasticSearchHandler {
             builder.field("data", event.getData());
             builder.field("type_tag", event.getTypeTag());
             builder.field("event_key", event.getEventKey());
-            builder.field("event_address", event.eventCreateAddress());
+            builder.field("event_address", eventAddress);
             if (struct != null) {
-                builder.field("tag_address", struct.getAddress());
-                builder.field("tag_module", struct.getModule());
-                builder.field("tag_name", struct.getName());
+                //add holder address
+                String tagName = struct.getName();
+                String tagAddress = struct.getAddress();
+                String tagModule = struct.getModule();
+                if (tagAddress.equals(Constant.EVENT_FILTER_ADDRESS) && tagModule.equals(Constant.EVENT_FILTER__MODULE)
+                        && (tagName.equalsIgnoreCase(Constant.DEPOSIT_EVENT) || tagName.equalsIgnoreCase(Constant.WITHDRAW_EVENT))) {
+                    try {
+                        DepositEvent inner = DepositEvent.bcsDeserialize(Hex.decode(event.getData()));
+                        String sb = inner.token_code.address +
+                                "::" +
+                                inner.token_code.module +
+                                "::" +
+                                inner.token_code.name;
+                        holders.add(new AddressHolder(eventAddress, sb));
+                    } catch (DeserializationError deserializationError) {
+                        logger.error("decode event data error:", deserializationError);
+                    }
+                }
+                builder.field("tag_address", tagAddress);
+                builder.field("tag_module", tagModule);
+                builder.field("tag_name", tagName);
             } else {
                 logger.warn("type tag is not struct: {}", event.getTypeTag());
             }
@@ -365,12 +449,13 @@ public class ElasticSearchHandler {
         return request;
     }
 
-    private void createIndexIfNotExist(String index) throws IOException {
-        String currentIndex = ServiceUtils.getIndex(network, index);
-        GetIndexRequest getRequest = new GetIndexRequest(currentIndex);
-        if (!client.indices().exists(getRequest, RequestOptions.DEFAULT)) {
-            CreateIndexResponse response = client.indices().create(new CreateIndexRequest(currentIndex), RequestOptions.DEFAULT);
-            logger.info("create index response: {}", response.toString());
+    static class AddressHolder {
+        private final String address;
+        private final String tokenCode;
+
+        AddressHolder(String address, String tokenCode) {
+            this.address = address;
+            this.tokenCode = tokenCode;
         }
     }
 }

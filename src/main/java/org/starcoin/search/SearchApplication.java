7b810169300af8039f4dc09348dfac1c605afc2e
chengsuoyuan <suoyuan@gmail.com>
2021-08-13 18:43:50 +0800

    # Conflicts: #	src/main/java/org/starcoin/search/handler/IndexerHandle.java

--- src/main/java/org/starcoin/search/SearchApplication.java
+++ src/main/java/org/starcoin/search/SearchApplication.java
@@ -1,16 +1,23 @@
 package org.starcoin.search;
 
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.starcoin.api.BlockRPCClient;
 import org.starcoin.api.StateRPCClient;
 import org.starcoin.api.TokenContractRPCClient;
 import org.starcoin.api.TransactionRPCClient;
+import org.starcoin.search.handler.RepairHandle;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -21,8 +28,61 @@ public class SearchApplication {
 
     public static void main(String[] args) {
         logger.info("start search service...");
-        SpringApplication.run(SearchApplication.class, args);
-        logger.info("APPLICATION FINISHED");
+        ConfigurableApplicationContext context = SpringApplication.run(SearchApplication.class, args);
+        if (args != null && args.length >= 2) {
+            RepairHandle repairHandle = (RepairHandle) context.getBean("repairHandle");
+            if (args[0].equals("repair")) {
+                long blockNumber = Long.parseLong(args[1]);
+                repairHandle.repair(blockNumber);
+            }
+            if (args[0].equals("check")) {
+                long begin = Long.parseLong(args[1]);
+                long end = Long.parseLong(args[2]);
+                repairHandle.check(begin, end);
+            }
+            if (args[0].equals("repair_file")) {
+                try {
+                    BufferedReader in = new BufferedReader(new FileReader(args[1]));
+                    String str;
+                    long blockNumber = 0;
+                    while ((str = in.readLine()) != null) {
+                        blockNumber = Long.parseLong(str);
+                        if (blockNumber > 0) {
+                            repairHandle.repair(blockNumber);
+                            System.out.println("repair ok :" + str);
+                        }
+                    }
+                    System.out.println("repair done");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (args[0].equals("auto_repair")) {
+                int count = 20;
+                long startNumber = Long.parseLong(args[1]);
+                while (startNumber > 0) {
+                    if (repairHandle.autoRepair(startNumber, count)) {
+                        startNumber += count;
+                    }
+                    try {
+                        Thread.currentThread().sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else {
+            //default mode
+            Scheduler scheduler = (Scheduler) context.getBean("scheduler");
+            if (scheduler != null) {
+                try {
+                    scheduler.start();
+                } catch (SchedulerException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
     @Bean(name = "base_url")
---
+++ src/main/java/org/starcoin/search/handler/RepairHandle.java
@@ -0,0 +1,157 @@
+package org.starcoin.search.handler;
+
+import com.thetransactioncompany.jsonrpc2.client.JSONRPC2SessionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.starcoin.api.BlockRPCClient;
+import org.starcoin.api.Result;
+import org.starcoin.api.TransactionRPCClient;
+import org.starcoin.bean.Block;
+import org.starcoin.bean.BlockHeader;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class RepairHandle {
+    private static Logger logger = LoggerFactory.getLogger(RepairHandle.class);
+
+    @Autowired
+    private ElasticSearchHandler elasticSearchHandler;
+
+    @Autowired
+    private BlockRPCClient blockRPCClient;
+
+    @Autowired
+    private TransactionRPCClient transactionRPCClient;
+
+    public void check(long begin, long end) {
+        for (long i = begin; i <= end; i++) {
+            try {
+                //get block from chain
+                Block blockOnChain = blockRPCClient.getBlockByHeight(i);
+                if (blockOnChain == null) {
+                    logger.warn("block not exist on chain: {}", i);
+                    continue;
+                }
+                //get block from es
+                Block blockOnEs = elasticSearchHandler.getBlockId(i);
+                if (blockOnEs != null) {
+                    if (!blockOnChain.getHeader().getBlockHash().equals(blockOnEs.getHeader().getBlockHash())) {
+                        System.out.println("check:" + i);
+                    }
+                } else {
+//                    logger.warn("es not exist block:{}", i);
+                    System.out.println("not exist: " + i);
+                }
+
+            } catch (JSONRPC2SessionException e) {
+                logger.error("repair error:", e);
+            }
+        }
+        logger.info("check ok");
+    }
+
+    public boolean autoRepair(long startNumber, int count) {
+        //read current master height
+        try {
+            BlockHeader chainHeader = blockRPCClient.getChainHeader();
+            long currentMasterNumber = chainHeader.getHeight();
+            if (startNumber > currentMasterNumber - 200) {
+                logger.info("repair too fast: {}", startNumber);
+                return false;
+            }
+        } catch (JSONRPC2SessionException e) {
+            logger.error("get master error: ", e);
+            return false;
+        }
+
+        List<Block> blocksOnChain;
+        try {
+            blocksOnChain = blockRPCClient.getBlockListFromHeight(startNumber, count);
+
+        } catch (JSONRPC2SessionException e) {
+            logger.error("get blocks on chain error:", e);
+            return false;
+        }
+        if (blocksOnChain.isEmpty()) {
+            logger.warn("blocks on chain is empty!");
+            return false;
+        }
+        Result<Block> blockResult = elasticSearchHandler.getBlockIds(startNumber - count, count);
+        if (blockResult == null || blockResult.getContents().isEmpty()) {
+            logger.warn("blocks on es is empty!");
+            return false;
+        }
+        Map<Long, Block> blockHashMap = new HashMap<>();
+        for (Block block : blockResult.getContents()) {
+            blockHashMap.put(block.getHeader().getHeight(), block);
+        }
+
+        List<Block> blockList = new ArrayList<>();
+        long newStartNumber = startNumber;
+        for (Block block : blocksOnChain) {
+            Block esBlock = blockHashMap.get(block.getHeader().getHeight());
+            if (esBlock == null) {
+                logger.warn("es block not exist: {}", block.getHeader().getHeight());
+                try {
+                    ServiceUtils.addBlockToList(transactionRPCClient, blockList, block);
+                } catch (JSONRPC2SessionException e) {
+                    logger.error("add block err:", e);
+                }
+                continue;
+            }
+            if (!block.getHeader().getBlockHash().equals(esBlock.getHeader().getBlockHash())) {
+                // fork block
+                try {
+                    ServiceUtils.addBlockToList(transactionRPCClient, blockList, block);
+                } catch (JSONRPC2SessionException e) {
+                    logger.error("add fix block err:", e);
+                }
+            } else {
+                logger.info("normal block: {}", block.getHeader().getHeight());
+            }
+            newStartNumber = block.getHeader().getHeight();
+        }
+        if (!blockList.isEmpty()) {
+            elasticSearchHandler.updateBlock(blockList);
+            logger.info("repair ok: {}", newStartNumber);
+        } else {
+            logger.info("not repair:{}", startNumber);
+        }
+        return true;
+    }
+
+    public void repair(long blockNumber) {
+        try {
+            //get block from chain
+            Block blockOnChain = blockRPCClient.getBlockByHeight(blockNumber);
+            if (blockOnChain == null) {
+                logger.warn("block not exist on chain: {}", blockNumber);
+                return;
+            }
+            //get block from es
+            Block blockOnEs = elasticSearchHandler.getBlockId(blockNumber);
+            if (blockOnEs != null) {
+                if (!blockOnChain.getHeader().getBlockHash().equals(blockOnEs.getHeader().getBlockHash())) {
+//                update block
+                    List<Block> blockList = new ArrayList<>();
+                    ServiceUtils.addBlockToList(transactionRPCClient, blockList, blockOnChain);
+                    elasticSearchHandler.updateBlock(blockList);
+                    logger.info("repair ok: {}", blockNumber);
+                } else {
+                    logger.info("not repair");
+                }
+            } else {
+                logger.warn("es not exist block:{}", blockNumber);
+            }
+
+        } catch (JSONRPC2SessionException e) {
+            logger.error("repair error:", e);
+        }
+    }
+}
